58d6564daada4ea3988a552ec3fa7e7166d4fa88
MubashirR <ansarimubashir47@gmail.com>
2024-12-21 06:08:12 +0500

    Added Write Client token and Query

--- components/ViewCount.tsx
+++ components/ViewCount.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import Ping from './Ping'
 import { StartViewCount } from '@/sanity/lib/queries'
 import { client } from '@/sanity/lib/client'
+import { writeClient } from '@/sanity/lib/write-client'
+import { after } from 'next/server'
 const ViewCount = async ({id} : {id: string}) => {
 
     const {viewCount} = await client.withConfig({useCdn: false}).fetch(StartViewCount, {id})
+
+   after(async () => {
+    await writeClient.patch(id).inc({viewCount: 1}).commit()
+   })
+
   return (
     <div className='view-container'>
         <div className='absolute -top-2 -right-2'>
